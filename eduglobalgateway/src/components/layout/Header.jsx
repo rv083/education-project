d1c68371560f4a7b639f018dd299536481563b14
Manan Bagga <mananbagga0310@gmail.com>
2025-11-21 19:00:55 +0530

    Open Pull Request

--- eduglobalgateway/src/components/layout/Header.jsx
+++ eduglobalgateway/src/components/layout/Header.jsx
@@ -1,4 +1,5 @@
 // src/components/layout/Header.jsx
+// Header + Topbar
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
