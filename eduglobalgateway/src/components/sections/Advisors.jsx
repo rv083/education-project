a717b8353b2186275e53150c89f92c706a7265ae
rv083 <rakshitvyas2003@gmail.com>
2025-12-04 19:29:48 +0530

    advisors change (only text no layout )

--- eduglobalgateway/src/components/sections/Advisors.jsx
+++ eduglobalgateway/src/components/sections/Advisors.jsx
@@ -15,7 +15,7 @@ const advisors = [
   {
     id: 1,
     name: "Prof. (Dr.) Vikram Singh",
-    title: "Retired Director General of Police (DGP) – Uttar Pradesh",
+    title: "Strategic Advisor",
     image: vikramImg,
     bio: `Retired Director General of Police (DGP) Uttar Pradesh, Prof. (Dr.) Vikram Singh is a nationally acclaimed figure recognized for his unwavering commitment to curbing crime and upholding law and order. Renowned for his integrity, discipline, and character, he is a trusted voice on matters of public importance and an inspiring figure for people of all generations, particularly the youth. EduGlobal Gateway greatly values Prof. (Dr.) Vikram Singh for his vast experience, scholarly contributions, and insightful guidance, which play a pivotal role in shaping the strategic decisions and vision of EduGlobal Gateway Consultancy.
 
@@ -39,19 +39,27 @@ Dr. Kakkar has taught and mentored thousands of students and authored three book
     bio: `Mr. Saurabh Mohan brings over 20 years of hands-on experience in overseas admissions, having successfully guided countless students to prestigious universities around the world. His deep expertise in the admissions process, program selection, and aligning academic choices with industry demands makes him an invaluable asset at EduGlobal Gateway Consultancy. By providing personalized guidance and strategic insights, he empowers students to make informed decisions, helping them secure placements in highly competitive undergraduate and postgraduate programs. His experience and mentorship serve as a strong guiding force, ensuring EduGlobal Gateway students choose the best-fit universities for their academic and career aspirations.`
   },
 
-  {
+ {
     id: 4,
+    name: "Mr. Aditya Rana",
+    title: "Program Director – Cognizant USA",
+    image: adityaImg,
+    bio: `Mr. Aditya has over 20 years of IT consulting experience, working with leading organizations such as Cognizant, Tata Consultancy Services (TCS), and Nucleus Software. His expertise spans multiple domains, including Retail, Pharmacy, Banking, and Hospitality. A forward-thinking technology leader, he excels at integrating modern technology stacks — including Generative AI — to drive innovation and enable business transformation. His strategic mindset and deep industry experience make him a key contributor in shaping future-ready enterprise solutions.
+
+His experience with global enterprises allows him to mentor students on selecting programs that not only match their academic goals but also maximize their career prospects internationally and help students understand how to position their profiles to meet admission requirements.`
+  },
+{
+    id: 5,
     name: "Ms. Sheena Shukla",
     title: "Commercial Strategy & Operations Leader – Google Cloud",
     image: sheenaImg,
     bio: `Ms. Sheena Shukla is a seasoned commercial strategy and operations leader with 10+ years of experience driving transformation across global organizations. At Google Cloud, she shapes high-impact programs that translate complex commercial goals into measurable business outcomes. Known for her expertise in deal strategy, data-driven decisioning, and cross-functional leadership, Ms. Sheena brings clarity, precision, and momentum to every initiative she leads. 
 Her strong business acumen and experience across global markets enable her to provide invaluable guidance to students and professionals.`
   },
-
   {
-    id: 5,
+    id: 6,
     name: "Mr. Mayank Bhati",
-    title: "MarTech & Customer Experience Product Leader",
+    title: "MarTech & Customer Experience Product Leader – Sprinklr",
     image: mayankImg,
     bio: `Mr. Mayank Bhati is a MarTech and Customer Experience Product Leader with 10+ years of experience in the banking and IT industry who has spent the last decade shaping AI-powered digital products for global enterprises. With a background spanning product strategy, analytics, and large-scale SaaS implementations, he has delivered transformative solutions across markets.At Sprinklr and ALDI DX, he led global marketing strategy through AI-driven VoC innovation, helping brands listen smarter and act faster.
 
@@ -59,7 +67,7 @@ Mr. Bhati's extensive international exposure, expertise in AI-powered products, 
   },
 
   {
-    id: 6,
+    id: 7,
     name: "Ms. Vandana Pareek",
     title: "Senior Business Consultant – Cleargoals, Canada",
     image: vandanaImg,
@@ -69,23 +77,13 @@ In addition to her achievements, she is deeply committed to guiding students and
   },
 
   {
-    id: 7,
+    id: 8,
     name: "Mr. Siddharth Vyas",
-    title: "Enterprise Solutions Architect – Cloud, SaaS & IIoT",
+    title: "Enterprise Solutions Architect – TCS, Canada",
     image: siddharthImg,
     bio: `With over 13 years of experience in business engagement, software development, and solution delivery across globally diverse environments, Mr. Vyas brings deep expertise in driving transformative digital initiatives. He has worked extensively with distributed, cross-functional teams to implement innovative solutions that address complex business challenges.Specializing in Cloud, SaaS, PaaS, and Industrial Internet of Things (IIoT) technologies, he has successfully delivered impactful solutions to more than 100 global customers. His strong ability to lead collaborative sessions with business stakeholders, product owners, development teams, and end-users ensures that every solution is aligned with real-world needs.
 
 In addition to his professional achievements, Mr. Vyas is passionate about guiding students in their study-abroad aspirations. His global exposure and experience working across diverse cultures allow him to offer valuable insights into international education pathways, career planning, and adapting to global work environments. He actively supports students in making informed decisions about courses, universities, and career opportunities, helping them prepare confidently for their academic journey overseas.`
-  },
-
-  {
-    id: 8,
-    name: "Mr. Aditya Rana",
-    title: "Program Director – Cognizant USA",
-    image: adityaImg,
-    bio: `Mr. Aditya has over 20 years of IT consulting experience, working with leading organizations such as Cognizant, Tata Consultancy Services (TCS), and Nucleus Software. His expertise spans multiple domains, including Retail, Pharmacy, Banking, and Hospitality. A forward-thinking technology leader, he excels at integrating modern technology stacks — including Generative AI — to drive innovation and enable business transformation. His strategic mindset and deep industry experience make him a key contributor in shaping future-ready enterprise solutions.
-
-His experience with global enterprises allows him to mentor students on selecting programs that not only match their academic goals but also maximize their career prospects internationally and help students understand how to position their profiles to meet admission requirements.`
   },
 ];
 
